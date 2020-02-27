3b0b626f5050f54f2e3dbd89c6b6718406061e60
Tatiana <45382631+Tatfi32@users.noreply.github.com>
2020-02-27 22:24:42 +0300

    Revert "Revert "CSV as NUMPY""
    This reverts commit 8fbcba720f30be2a67e01b90921b0842b6b8b102.

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from pcap_reader import *
 from yaml_reader import *
 from logger import *
-from smi_calculation import *
+from smi_calculation_new import *
 
 if __name__ == '__main__':
     """
@@ -27,8 +27,8 @@ if __name__ == '__main__':
             save image and correspond lidar data from one shot
             
     """
-
     #local_yml_reader = YML_Read(Path().absolute())
-    # local_yml_reader.power()
+    #local_yml_reader.power()
+
     local_calibrator = SMI_calculations(Path().absolute()/ 'results')
     local_calibrator.power()
