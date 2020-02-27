bf84a833ab92ec484ac5dd6842ad3a572cccf2b3
Tatiana <45382631+Tatfi32@users.noreply.github.com>
2020-02-27 22:26:13 +0300

    Revert "Revert "Revert "Revert "Revert "CSV as NUMPY"""""
    This reverts commit a3f817f2697ab41811fb3cdf9848ce6e01581d8d.

--- logger.py
+++ logger.py
@@ -3,15 +3,16 @@ from distutils.dir_util import copy_tree
 import shutil
 from datetime import datetime
 
+
 class LogWriter:
     def __init__(self, data_path):
 
         self.data_path = data_path
         self.results_path = Path(data_path) / 'results'
-        self.origin_image_path = Path(data_path) / 'data' / 'frames'
         if self.results_path.is_dir():
             shutil.rmtree(str(self.results_path), ignore_errors=True)
         self.results_path.mkdir()
+
         self.calib_path = self.results_path / 'calib'
         self.image_path = self.results_path / 'leftImage'
         self.lidar_path = self.results_path / 'velodyne_points'
@@ -28,25 +29,23 @@ class LogWriter:
         open(str(self.lidar_path / 'timestamps.txt'), "w+")
         copy_tree(str(Path(self.data_path) / 'data' / 'frames' / 'calib'), str(self.calib_path))
 
-        self.origin_image_files = [x for x in sorted(self.origin_image_path.glob('*.bmp')) if x.is_file()]
-        self.origin_image_names = [x.name for x in self.origin_image_files]
+        self.image_files = [x for x in (Path(self.data_path) / 'data' / 'frames').glob('*.bmp') if x.is_file()]
         self.indx = 0
 
     def save_images(self, yaml_img_name, image_time):
-
-        if yaml_img_name in self.origin_image_names:
-            path = self.origin_image_path / str(yaml_img_name)
-            shutil.copy(str(path), str(self.image_data_path))
-            print('Saving camera data, file name =', yaml_img_name)
-
+        for item in self.image_files:
+            if item.stem == yaml_img_name:
+                print('Saving camera data, file name =', yaml_img_name)
+                shutil.copy(item, str(self.image_data_path))
+        """
         with open(str(self.image_path / 'timestamps.txt'), "a+") as f:
             f.write("%s\n" % datetime.fromtimestamp(image_time).strftime('%Y-%m-%d %H:%M:%S.%f'))
+        """
 
-
-    def save_lidar_data(self, time_lidar, df, name):
+    def save_lidar_data(self, time_lidar, df):
         # print('Saving lidar data, file number =', self.indx)
         with open(str(self.lidar_path / 'timestamps.txt'), "a+") as f:
             f.write("%s\n" % time_lidar)
-        path = (str(self.lidar_data_path / name))
+        path = (str(self.lidar_data_path / str(self.indx)) + '.csv')
         df.to_csv(path, header=False, index=False)
         self.indx += 1
